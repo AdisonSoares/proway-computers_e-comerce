76a97bec3fbef0eb0f71066cfca398d1b04ece8e
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2023-09-24 10:06:36 -0300

    :sparkles: Feat: adds routing navigation

---
+++ proway-computers/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Router, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports:[
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
