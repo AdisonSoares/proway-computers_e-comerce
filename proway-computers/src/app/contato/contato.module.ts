46b71550170b6e2a606d527c2ccbb155095539ec
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2023-09-25 03:10:30 -0300

    :sparkles: Feat: adds contact page validation

--- proway-computers/src/app/contato/contato.module.ts
+++ proway-computers/src/app/contato/contato.module.ts
@@ -3,15 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ContatoRoutingModule } from './contato-routing.module';
 import { ContatoComponent } from './contato.component';
-
-
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     ContatoComponent
   ],
   imports: [
     CommonModule,
-    ContatoRoutingModule
+    ContatoRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ContatoModule { }
