d3a84867e2174b5a4ff1c0a79cdc17b214aa3c6c
Adison Soares <62489704+AdisonSoares@users.noreply.github.com>
2023-09-26 14:55:37 -0300

    :sparkles: Feat: enter settings

--- proway-computers/src/app/app-routing.module.ts
+++ proway-computers/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 const routes: Routes = [
   { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
   {path: "", redirectTo: "produtos", pathMatch: "full"},
+  {path: "**", component: NaoEncontradaComponent},
   { path: 'carrinho', loadChildren: () => import('./carrinho/carrinho.module').then(m => m.CarrinhoModule) },
   { path: 'contato', loadChildren: () => import('./contato/contato.module').then(m => m.ContatoModule) },
-  {path: "**", component: NaoEncontradaComponent}
 ];
 
 @NgModule({
